a2157a87a0b1d017f3204e3714b760ec8b77e984
grettaelizabeth <gretta@gmail.com>
2022-10-17 08:19:44 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+// textEntryDiv.innerHTML += `${fruitPhonetics}`
+
+function fetchDict() {
+    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/fruit')
+     .then((response) => {
+      if (!response.ok) {
+       throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response.text();
+     })
+     .then((text) => return text)
+     .catch((error) => return `Could not fetch dictionary: ${error}`);
+}
+
 let textEntryDiv = document.getElementById('text-entry')
 textEntryDiv.innerHTML += '<p> frog and toad </p>'
-fetch('https://api.dictionaryapi.dev/api/v2/entries/en/fruit')
- .then(res => res.text().split(",")[1])
- .then(fruitPhonetics => {
-     textEntryDiv.innerHTML += `${fruitPhonetics}`
-})
+let fullText = fetchDict()
+textEntryDiv.innerHTML += `<p> $fullText </p>`
