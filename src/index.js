daf833318980ae2ea7e068c32cbe48e3a3ea06a1
grettaelizabeth <gretta@gmail.com>
2022-10-16 18:51:15 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 let textEntryDiv = document.getElementById('text-entry')
 textEntryDiv.innerHTML += '<p> frog and toad </p>'
-fetch('https://api.dictionaryapi.dev/api/v2/entries/en/fruit')
-// .then(dict => {textEntryDiv.innerHTML += '<p> ${dict} </p>'},)
-   .then(textEntryDiv.innerHTML += '<p> success! </p>',
-         textEntryDiv.innerHTML += '<p> failure! </p>')
+let fruitDef = fetch('https://api.dictionaryapi.dev/api/v2/entries/en/fruit')
+textEntryDiv.innerHTML += fruitDef
