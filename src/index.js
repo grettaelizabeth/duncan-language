607c3dda94df223d8dd86fb6be8b5c6a161c5214
grettaelizabeth <gretta@gmail.com>
2022-10-16 19:00:51 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ textEntryDiv.innerHTML += '<p> frog and toad </p>'
 fetch('https://api.dictionaryapi.dev/api/v2/entries/en/fruit')
  .then()
  .then(fruitDef => {
-     textEntryDiv.innerHTML += fruitDef
+     textEntryDiv.innerHTML += fruitDef[1]
 })
